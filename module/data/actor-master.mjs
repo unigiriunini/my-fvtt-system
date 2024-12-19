e9cac91dcfee5cf80c02d58586af6d1c3dc9a76e
Unigiri Unini <me@unigiri.net>
2024-12-20 05:43:00 +0900

    feat: Define fields for Master
    These fields are sufficient to define in the Data Model for Master
    since they are only used by it

--- module/data/actor-master.mjs
+++ module/data/actor-master.mjs
@@ -4,11 +4,9 @@ export default class HolyGrailWarTRPGMaster extends HolyGrailWarTRPGActorBase {
 
   static defineSchema() {
     const fields = foundry.data.fields;
-    const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
     schema.name = new fields.StringField({ required: true, blank: true });
-    schema.fame = new fields.StringField({ required: true, blank: true });
     schema.wish = new fields.StringField({ required: true, blank: true });
 
     return schema;
